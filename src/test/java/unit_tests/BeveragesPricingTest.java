0698e4c55703fea5f2c296ce9c4d4cb70d287dde
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-11-28 13:16:54 +0100

    Refactor to use composition

--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -2,6 +2,11 @@ package unit_tests;
 
 import beverages.*;
 import org.junit.Test;
+import supplements.Cream;
+import supplements.Milk;
+import supplements.Supplement;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,25 +33,29 @@ public class BeveragesPricingTest {
 
     @Test
     public void computes_tea_with_milk_price() {
-        Tea teaWithMilk = new TeaWithMilk();
+        List<Supplement> supplements = List.of(new Milk());
+        Beverage teaWithMilk = new Tea(supplements);
         assertThat(teaWithMilk.price(), is(closeTo(1.60, 0.001)));
     }
 
     @Test
     public void computes_coffee_with_milk_price() {
-        Coffee coffeeWithMilk = new CoffeeWithMilk();
+        List<Supplement> supplements = List.of(new Milk());
+        Beverage coffeeWithMilk = new Coffee(supplements);
         assertThat(coffeeWithMilk.price(), is(closeTo(1.30, 0.001)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cream_price() {
-        Coffee coffeeWithMilkAndCream = new CoffeeWithMilkAndCream();
+        List<Supplement> supplements = List.of(new Milk(), new Cream());
+        Beverage coffeeWithMilkAndCream = new Coffee(supplements);
         assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, 0.001)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_price() {
-        HotChocolateWithCream hotChocolateWithCream = new HotChocolateWithCream();
+        List<Supplement> supplements = List.of(new Cream());
+        Beverage hotChocolateWithCream = new HotChocolate(supplements);
         assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, 0.001)));
     }
 }
