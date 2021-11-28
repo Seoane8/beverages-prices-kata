57007da1e012647bd8a87d9f54569de27bd290fb
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-11-28 13:30:10 +0100

    Adds Cinnamon supplement

---
+++ src/main/java/supplements/Cinnamon.java
@@ -0,0 +1,10 @@
+package supplements;
+
+public class Cinnamon extends Supplement{
+
+    private static final double PRICE = 0.05;
+
+    public Cinnamon() {
+        super(PRICE);
+    }
+}
--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -2,6 +2,7 @@ package unit_tests;
 
 import beverages.*;
 import org.junit.Test;
+import supplements.Cinnamon;
 import supplements.Cream;
 import supplements.Milk;
 import supplements.Supplement;
@@ -57,5 +58,54 @@ public class BeveragesPricingTest {
         List<Supplement> supplements = List.of(new Cream());
         Beverage hotChocolateWithCream = new HotChocolate(supplements);
         assertThat(hotChocolateWithCream.price(), is(closeTo(1.60, 0.001)));
+    }
+
+    @Test
+    public void computes_coffee_with_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Cinnamon());
+        Beverage coffeeWithCinnamon = new Coffee(supplements);
+        assertThat(coffeeWithCinnamon.price(), is(closeTo(1.25, 0.001)));
+    }
+
+    @Test
+    public void computes_tea_with_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Cinnamon());
+        Beverage teaWithCinnamon = new Tea(supplements);
+        assertThat(teaWithCinnamon.price(), is(closeTo(1.55, 0.001)));
+    }
+
+    @Test
+    public void computes_hot_chocolate_with_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Cinnamon());
+        Beverage hotChocolateWithCinnamon = new HotChocolate(supplements);
+        assertThat(hotChocolateWithCinnamon.price(), is(closeTo(1.50, 0.001)));
+    }
+
+    @Test
+    public void computes_tea_with_milk_and_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Milk(), new Cinnamon());
+        Beverage teaWithMilk = new Tea(supplements);
+        assertThat(teaWithMilk.price(), is(closeTo(1.65, 0.001)));
+    }
+
+    @Test
+    public void computes_coffee_with_milk_and_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Milk(), new Cinnamon());
+        Beverage coffeeWithMilk = new Coffee(supplements);
+        assertThat(coffeeWithMilk.price(), is(closeTo(1.35, 0.001)));
+    }
+
+    @Test
+    public void computes_coffee_with_milk_and_cream_and_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Milk(), new Cream(), new Cinnamon());
+        Beverage coffeeWithMilkAndCream = new Coffee(supplements);
+        assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.50, 0.001)));
+    }
+
+    @Test
+    public void computes_hot_chocolate_with_cream_and_cinnamon_price() {
+        List<Supplement> supplements = List.of(new Cream(), new Cinnamon());
+        Beverage hotChocolateWithCream = new HotChocolate(supplements);
+        assertThat(hotChocolateWithCream.price(), is(closeTo(1.65, 0.001)));
     }
 }
